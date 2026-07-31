076a22a135dd933fc8bb183032fde22235012fa6
Vetri L <vsvetri9@gmail.com>
2026-08-01 00:19:49 +0530

    Add files via upload

---
+++ practice_issorted.php
@@ -0,0 +1,20 @@
+<?php
+    $array=[12,34,55,66,90,99];
+   /* $issorted=true;
+    for($i=0;$i<count($array);$i++){
+        $current=$i;
+        $next=$i+1;
+        if($array[$current]>$array[$next]){
+            $issorted=false;
+            break;
+        }
+        $i++;
+    }
+    if($issorted){
+        echo "GIVEN ARRAY IS SORTED";
+    }
+    else{
+        echo "GIVEN ARRAY IS UNSORTED";
+    }*/
+    
+?>
